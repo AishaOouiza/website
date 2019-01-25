29d10163a4cdd39f93a2d0b02062312ed0419c82
AishaOouiza <46681001+AishaOouiza@users.noreply.github.com>
2019-01-25 01:52:14 +0100

    Add files via upload

--- vehicle.js
+++ vehicle.js
@@ -1,19 +1,17 @@
-// CLASS VEHICLE
-class vehicle {
+// CLASS DEFINITION
+class Car {
     
     // CONSTRUCTOR
-    constructor(type, color, model, vin) {
-        this.type = type;
+    constructor(color, model, vin) {
         this.color = color;
         this.model = model;
         this.vin = vin;
-        
-        
+        this.year = new Date().getFullYear(); // ENCAPSULATION
+        this.brand = "Generic Car";
+        this.mileage = 0;
     }
-   //GETTERS
-    getType(){
-       return this.type;
-    }
+
+    // GETTERS
     getColor() {
         return this.color;
     }
@@ -26,15 +24,15 @@ class vehicle {
         return this.vin;
     }
 
+    getYear() {
+        return this.year;
+    }
+
     getBrand() {
         return this.brand;
     }
 
-
     // SETTERS
-    setType(type) {
-        this.type = type;
-    }
     setColor(color) {
         this.color = color;
     }
@@ -47,75 +45,41 @@ class vehicle {
         this.vin = vin;
     }
 
+    setYear(year) {
+        this.year = year;
+    }
+
     setBrand(brand) {
         this.brand = brand;
     }
- 
-// PARENT CLASS
-class vehicle {
-    constructor(color, model, vin) {
-    this.color =red;
-    this.model = 2014;
-    this.vin = 239840343;
-
 }
 
 // INHERITANCE
-class Car extends Vehicle {
+class ToyotaCar extends Car {
     constructor(color, model, vin) {
         super(color, model, vin); // PARENT CLASS SATISFACTION
-        this.type = Car
         this.brand = "Toyota";
-        this.noOfTires = 4;
-        this.bulletProof = True;
-
-        // INHERITANCE
-class Bus extends Vehicle {
-    constructor(color, model, vin) {
-        super(color, model, vin); // PARENT CLASS SATISFACTION
-        this.type = Bus
-        this.brand = "Hiace";
-        this.noOfTires = 4;
+        this.noOfTires = 6;
         this.bulletProof = false;
+    }
 
-        // INHERITANCE
-class Truck extends Vehicle {
-    constructor(color, model, vin) {
-        super(color, model, vin); // PARENT CLASS SATISFACTION
-        this.type = Truck
-        this.brand = "Man";
-        this.noOfTires = 12;
-        this.bulletProof = false;
+    aboutThisCar() {
+        console.log("My ", this.getBrand(), " car has a color of ", 
+                this.getColor(), " and it VIN is: ", this.getVin());
+    }
+}
 
-        aboutThisVehicle() {
-            console.log("My ", this.getType(), " vehicle has a color of ", 
-                    this.getColor(), " it VIN is: ", this.getVin(),"it is a ", this.brand),
-                    "with", this.noOfTires;
-        }
-    }     
+// INITIALIZING THE CAR OBJECT
+let toyotaCar = new Car("Red", "Venza", 346387463876);
 
-    
-    let toyotaCar = new Car("Red", "Venza", 346387463876);
-    let HiaceBus = new Bus ("blue","Serena", 77877677867);
-    let ManTruck = new Truck ("white, bloom",35432434545);
+// INTERACTING WITH THE TOYOTA CAR
+console.log("My Toyota", toyotaCar.getModel(), " car has a color of ", 
+                toyotaCar.getColor(), " and it VIN is: ", toyotaCar.getVin());
 
-    console.log("My Toyota", toyotaCar.getModel(), " car has a color of ", 
-                toyotaCar.getColor(), " and it VIN is: ", toyotaCar.getVin(), "it is a", toyotaCar.getBrand(), "with", toyotaCar.getNoOfTyres());
+// INTERACTED WITH THE BABY CLASS
+let babyToyotaCar = new ToyotaCar("Yellow", "Corolla", 39874893479837);
+babyToyotaCar.aboutThisCar();
 
-    console.log("My Hiace", Hiacebus.getModel(), " bus has a color of ", 
-                HiaceBus.getColor(), " and it VIN is: ", HiaceBus.getVin(),"it is a", HiaceBus.getBrand(), "with", HiaceBus.getNoOfTyres());
-                
-
-    console.log("My Man", ManTruck.getModel(), " truck has a color of ", 
-                ManTruck.getColor(), " and it VIN is: ", ManTruck.getVin(), "it is a", ManTruck.getBrand(), "with", ManTruck.getNoOfTyres());
-            
-
-
-
-
-            
-            
-
-
- 
-
+// DESTROY OBJECTS
+delete toyotaCar, babyToyotaCar;
+console.log(babyToyotaCar.aboutThisCar());
